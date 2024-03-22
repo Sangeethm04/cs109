6d6beeb700cccf220faf73a1d4d623288c096467
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2024-03-21 23:45:00 -0400

    ev

--- test.c
+++ test.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
-int main() {
- int a = '5' - '0';
-    printf("%d\n", a);
-    printf("%d\n", a);
-    return 0;
+void modifyValue(int ptr) {
+    ptr = 10;  // Modifying the value at the memory location pointed to by ptr
 }
 
+int main() {
+   int num;
+   char chars;
 
+    printf("Enter an integer: ");
+    scanf("%d %c", &num, &chars);  // Reads an integer from standard input
+    num = 45;
+    printf("You entered: %d\n", num);
+    printf("You entered: %c\n", chars);
 
+    return 0;
 
-
+}
